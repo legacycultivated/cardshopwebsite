7862fb7cc811132bb12daac2d605b62544bfcb51
Lyman Chan <lyman.chan@hotmail.com>
2025-05-27 21:01:02 -0700

    Initial commit with Create React App and Tailwind CSS

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from "./logo.svg";
+import "./App.css";
+function App() {
+  return (
+    <div className="text-blue-600 text-3xl font-bold p-8">
+      Tailwind CSS v3 is working!
+    </div>
+  );
+}
+
+export default App;
